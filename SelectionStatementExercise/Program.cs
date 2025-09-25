965a99f942ccde247fed8bf4d104df5c7ccfe8f7
Evelyn Puga <pugaevelyn0@gmail.com>
2025-09-25 14:05:52 -0700

    Completed exercise 1.

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var random = new Random();
+            var favNumber = random.Next(1, 11);
+            int userInput = 0;
+
+            while (userInput != favNumber)
+            {
+                Console.WriteLine("Guess my favorite number between 1 and 10:");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput > favNumber)
+                {
+                    Console.WriteLine($"Too high!\nYour guess: {userInput}");
+                }
+                else if (userInput < favNumber)
+                {
+                    Console.WriteLine($"Too low! Your guess: {userInput}");
+                }
+                else 
+                {
+                    Console.WriteLine($"You guessed it! The favorite number was {userInput}!");
+                }  
+            }
+            
         }
     }
 }
